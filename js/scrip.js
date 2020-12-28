5a62dd063614b70f81036d2e0b3d6cd05f48e902
Raihananr <raihananrr@gmail.com>
2020-12-28 21:47:34 +0700

    pesan commit

---
+++ js/scrip.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Raihan Anhar"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+});
